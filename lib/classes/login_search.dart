23293ed759771bb9743d467ad11857a4fb088ec4
refilwefiefie <93614957+refilwefiefie@users.noreply.github.com>
2021-11-11 15:08:21 +0200

    Delete login_search.dart

--- lib/classes/login_search.dart
+++
@@ -1,6 +0,0 @@
-//Name : LoginSearch
-//Purpose : The LoginSearch class will be used to verify that a
-//          user's profile exists on the system upon a login attempt
-class LoginSearch {
-  void search() {}
-}
